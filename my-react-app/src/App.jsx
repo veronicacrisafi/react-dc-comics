e9bb143ab817d8b7a68ea183cbe7d19c9b3069d2
veronicacrisafi <veronicacrisafi3@gmail.com>
2025-09-21 14:53:10 +0200

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,21 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import dcLogo from './assets/img/dc-logo.png'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <header>
+        <img src={dcLogo} alt="DC-logo-Header"/>
+      </header>
+      <main></main>
+      <footer></footer>
+    </>
+  )
+}
+
+export default App
