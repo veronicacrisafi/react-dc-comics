23a0f5c8389a47992aa910aeb140d0d889231c73
veronicacrisafi <veronicacrisafi3@gmail.com>
2025-09-24 22:18:05 +0200

    inserito il button load more sotto le card

--- my-react-app/src/components/AppMain.jsx
+++ my-react-app/src/components/AppMain.jsx
@@ -54,6 +54,11 @@ export default function AppMain() {
                             <AppFumetti imgURL="https://imgs.search.brave.com/bf_FlGnymAgmvweY8YiLAcyy600JdSMl_jHpdR3t5ro/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvNTA5/M2IxOTEtZTkzZS00/NTJjLThhYTQtYTEw/ZDY0ZTcxN2E5LjA5/N2UwZWM5NDgzYzky/OGExMjY4Nzk1Nzk2/NTliZWNjLmpwZWc_/b2RuV2lkdGg9NjEy/Jm9kbkhlaWdodD02/MTImb2RuQmc9ZmZm/ZmZm" title="catwoman" />
                         </div>
                     </div>
+                    <div className='text-center'>
+                        <a href="" className="btn btn-primary text-uppercase rounded-0 mb-4" target="_blank" rel="noopener noreferrer">
+                            load more
+                        </a>
+                    </div>
                 </div>
             </div>
             <div className='main-bg'>
