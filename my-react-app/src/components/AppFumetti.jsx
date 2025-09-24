c0263b018d08bf19fbaa10ef664ce341198c43c9
veronicacrisafi <veronicacrisafi3@gmail.com>
2025-09-24 21:56:02 +0200

    finito di sistemare le card

--- my-react-app/src/components/AppFumetti.jsx
+++ my-react-app/src/components/AppFumetti.jsx
@@ -1,9 +1,9 @@
 export default function AppFumetti({ imgURL, title }) {
     return (
         <div className="">
-            <img src={imgURL} alt="fumetti-main" className="img-fluid rounded" style={{ width: "150px", height: "200px", objectFit: "cover" }} ></img>
+            <img src={imgURL} alt="fumetti-main" className="img-fluid" style={{ width: "200px", height: "200px", objectFit: "cover" }} ></img>
             <div className="">
-                <h6 className="text-light text-break text-uppercase" style={{ whiteSpace: "normal", overflowWrap: "break-word" }}>{title}</h6>
+                <h6 className="text-light text-break text-uppercase pt-3 pb-5" style={{ whiteSpace: "normal", overflowWrap: "break-word" }}>{title}</h6>
             </div>
         </div>
     )
