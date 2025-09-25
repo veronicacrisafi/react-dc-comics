22e0c6538b600552de39b5be69ba3b9028ebe0c0
veronicacrisafi <veronicacrisafi3@gmail.com>
2025-09-25 12:39:31 +0200

    in AppFumetti cambiato il primo div in un frammento

--- my-react-app/src/components/AppFumetti.jsx
+++ my-react-app/src/components/AppFumetti.jsx
@@ -1,10 +1,10 @@
 export default function AppFumetti({ imgURL, title }) {
     return (
-        <div className="">
+        <>
             <img src={imgURL} alt="fumetti-main" className="img-fluid" style={{ width: "200px", height: "200px", objectFit: "cover" }} ></img>
             <div className="">
                 <h6 className="text-light text-break text-uppercase pt-3 pb-5" style={{ whiteSpace: "normal", overflowWrap: "break-word" }}>{title}</h6>
             </div>
-        </div>
+        </>
     )
 }
