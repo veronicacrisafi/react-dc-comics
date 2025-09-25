d180e46e99d625f06b2372a247aeabb4be8c3ac7
veronicacrisafi <veronicacrisafi3@gmail.com>
2025-09-25 15:01:22 +0200

    inserito children in AppFumetti

--- my-react-app/src/components/AppFumetti.jsx
+++ my-react-app/src/components/AppFumetti.jsx
@@ -1,14 +1,15 @@
-export default function AppFumetti({ imgURL, title }) {
+export default function AppFumetti({ imgURL, title, children }) {
     return (
         <>
 
             <img src={imgURL} alt="fumetti-main" className="img-fluid" style={{ width: "200px", height: "200px", objectFit: "cover" }} />
             <div>
-                <h6 className="text-light text-break text-uppercase pt-3 pb-2 text-center" style={{ whiteSpace: "normal", overflowWrap: "break-word" }}>
+                <h6 className="text-light text-break text-uppercase pt-3 pb-2 text-start" style={{ whiteSpace: "normal", overflowWrap: "break-word" }}>
                     <a href="" className="text-decoration-none" target="_blank" rel="noopener noreferrer">
                         {title}
                     </a>
                 </h6>
+                {children}
             </div>
 
         </>
